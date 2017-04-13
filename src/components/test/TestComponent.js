951fb5c1046bbe714455d339e6127a27e07402d3
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-04-13 17:48:30 +0200

    added linking module

---
+++ src/components/test/TestComponent.js
@@ -0,0 +1,9 @@
+/**
+ *Created by Thomas on 13/04/2017.
+ * This is a component for testing rendering and linking with Travis
+ */
+import React from 'react'
+
+const TestComponent = () => <h1>I am a component for testing</h1>
+
+export default TestComponent
