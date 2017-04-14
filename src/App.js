d34cb73772bfcdc9de37d2722dc27a63fdb33995
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-04-14 08:50:33 +0200

    testing on osX

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import RootTest from './RootTestComponent'
 // import './styles/testStyles.css'
 
 class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2 className='my-class'>Welcome to React</h2>
-        </div>
-        <Test />
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <RootTest />
-      </div>
-    )
-  }
+    render() {
+        return ( <
+            div className = 'App' >
+            <
+            div className = 'App-header' >
+            <
+            img src = { logo }
+            className = 'App-logo'
+            alt = 'logo' / >
+            <
+            h2 className = 'my-class' > Welcome to React < /h2> <
+            /div> <
+            Test / >
+            <
+            p className = 'App-intro' >
+            To get started, edit < code > src / App.js < /code> and save to reload. <
+            /p> <
+            RootTest / >
+            <
+            /div>
+        )
+    }
 }
 
 export default App
