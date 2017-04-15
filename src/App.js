4b67ea5614082975b2b1ce2afb4c8a8ecb0a4db3
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-04-15 17:03:02 +0200

    PLayed around with flow

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import Test from './components/test/TestComponent'
-// import './App.css'
+import './App.css'
 import RootTest from './RootTestComponent'
-// import './styles/testStyles.css'
+import './styles/testStyles.css'
 
 class App extends Component {
-    render() {
-        return ( <
-            div className = 'App' >
-            <
-            div className = 'App-header' >
-            <
-            img src = { logo }
-            className = 'App-logo'
-            alt = 'logo' / >
-            <
-            h2 className = 'my-class' > Welcome to React < /h2> <
-            /div> <
-            Test / >
-            <
-            p className = 'App-intro' >
-            To get started, edit < code > src / App.js < /code> and save to reload. <
-            /p> <
-            RootTest / >
-            <
-            /div>
-        )
-    }
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2 className='my-class'>Welcome to React</h2>
+        </div>
+        <Test />
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <RootTest />
+      </div>
+    )
+  }
 }
 
 export default App
