3780c12f8c292008960f4f42136579b24f313b1e
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-04-17 10:54:32 +0200

    Wrote an action and some tests for setting counties.

--- src/redux/reducers/counties/countiesActions.js
+++ src/redux/reducers/counties/countiesActions.js
@@ -2,3 +2,6 @@
  * Created by Thomas Hartmann
  * Counties actions component
  */
+import types from './countiesActionTypes'
+
+export const setCounties = (newCounties = []) => ({ type: types.SET_COUNTIES, payload: newCounties })
--- src/redux/reducers/counties/countiesActions.test.js
+++ src/redux/reducers/counties/countiesActions.test.js
@@ -1,4 +1,26 @@
+/* global it, expect */
 /*
  * Created by Thomas Hartmann
  * Test component for counties actions
  */
+import { setCounties } from './countiesActions'
+import types from './countiesActionTypes'
+
+it('should return an action with the correct type and payload', () => {
+  const payload = [
+    "Oslo",
+    "Akershus",
+    "Vestfold"
+  ]
+  const expectedResult = {
+    type: types.SET_COUNTIES,
+    payload: payload
+  }
+  expect(setCounties(payload)).toEqual(expectedResult)
+})
+
+it('should return an empty result set if no argument is passed', () => {
+  const expectedResult = ({ type: types.SET_COUNTIES, payload: [] })
+
+  expect(setCounties()).toEqual(expectedResult)
+})
