a6fc4e5eabdafdd3573542bb80cce4cc2e3a5d89
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-04-17 10:13:16 +0200

    Added image defaults in case a project doesn't have any

--- src/components/boligvelger/components/searchResults/components/resultItem/ResultItem.js
+++ src/components/boligvelger/components/searchResults/components/resultItem/ResultItem.js
@@ -3,8 +3,9 @@
  * Component that lists details about a single result
  */
 import React from 'react'
+import defaultImage from '../../../../../../media/images/nybygg.jpg'
 
-export default ({ projectName, poststed, housingType, bedrooms, price }) => (
+export default ({ projectName, poststed, housingType, bedrooms, price, image, imgAlt }) => (
   <article className='housing-item-container'>
     <div className='housing-item-header row middle-xs'>
       <h2 className='housing-item-title col-xs start-xs'>{projectName}</h2>
@@ -23,7 +24,9 @@ export default ({ projectName, poststed, housingType, bedrooms, price }) => (
         </div>
       </div>
       <div className='col-xs-6 housing-item-image'>
-        <img className='img-responsive' src='~/Content/assets/img/nybygg.jpg' alt='Model.ProjectName' />
+        <img className='img-responsive'
+          src={image || defaultImage}
+          alt={imgAlt || projectName} />
       </div>
     </div>
   </article>
