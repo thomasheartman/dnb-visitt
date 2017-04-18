6a92296bf397ebf4c217afabf13d2a5033db1c3e
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-04-18 12:14:14 +0200

    Got connection. Next is filtering

--- src/redux/reducers/searchResults/resultsActions.js
+++ src/redux/reducers/searchResults/resultsActions.js
@@ -5,6 +5,7 @@
 
 import types from './resultsActionTypes'
 import { database } from '../../../firebase/firebase'
+import _ from 'lodash'
 
 export const startFetchingResults = () => ({ type: types.FETCH_RESULTS })
 
@@ -15,11 +16,50 @@ export const changeResults = results => ({ type: types.CHANGE_RESULTS, payload: 
 export const fetchResults = (parameters) => (dispatch, getState) => {
   if (getState().searchResults.fetchingResults) return
 
+  const filter = getState().filter
+
+  const db = database.ref('properties')
+
+  // debug
+  db.orderByKey().on("child_added", function (snapshot) {
+    console.log(snapshot.child('Fylke'))
+    console.log(snapshot.key)
+  })
+
   console.log('fetching results now')
 
-  dispatch(fetchResults)
-
-  database.ref('/properties/').once('value', snapshot => {
-    dispatch(changeResults(snapshot.val()))
-  })
+  if (_.isEmpty(filter.counties)) {
+    db.once('value', snapshot => {
+      dispatch(changeResults(snapshot.val()))
+    })
+  } else {
+    let results = []
+    Promise.all(
+      filter.counties.map(county => db.once('value', snapshot => {
+        console.log(`Getting results for ${county}`)
+        return db.child('Fylke').orderByValue().equalTo(county).once('value')
+      })
+        .then(snapshot => {
+          console.log(`Snapshot results: ${snapshot.val().length}`)
+          results = [...results, ...snapshot.val()]
+        }))
+    )
+      .then(() => {
+        console.log(`Final results: ${results}`)
+        dispatch(changeResults(results))
+      })
+  }
 }
+
+/*const loadVideosParallel = (counties, callback) => {
+  Promise.all(
+    counties.map(county => {
+      console.log('send request ' + county);
+      return db.child('properties').child(county).equalTo(county).once('value')
+        .then(snapshot => {
+          console.log('got response ' + county);
+          return snapshot;
+        })
+    })
+  ).then(r => callback());
+}*/
