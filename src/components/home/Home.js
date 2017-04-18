48a7f35e1edb09534d65a9300098afabcadb2077
Nikolai N <nikolainorge@gmail.com>
2017-04-18 15:52:48 +0200

    Change to new footer and diverse styling issues

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom'
 import AboutBox from './components/aboutBox/AboutBox'
 import Steps from './components/visitSteps/VisitSteps'
 
+
 export default ({ selectedCounties, allCounties, handleChange = f => f }) => (
   <main className='frontpage full-width'>
     <AboutBox />
@@ -15,15 +16,16 @@ export default ({ selectedCounties, allCounties, handleChange = f => f }) => (
       <div className='row'>
         <div className='col-xs-12 center-xs'>
           <h1 className='title-heading'>Hvor ønsker du å bo?</h1>
-          <div className='dropdown row center-xs' />
-          <select value={selectedCounties} onChange={(event) => handleChange(event.target.value)}>
-            {allCounties.map((county) =>
-              <option value={county} key={county}>{county}</option>
-            )}
-          </select><br />
-          <Link to='/boligvelger'>
-            <input type='submit' value='Søk' />
-          </Link>
+			<div className='dropdown row center-xs'>
+				<select value={selectedCounties} onChange={(event) => handleChange(event.target.value)}>
+					{allCounties.map((county) =>
+					  <option value={county} key={county}>{county}</option>
+					)}
+				</select><br />
+				<Link to='/boligvelger'>
+					<input type='submit' value='Søk' />
+				</Link>
+			</div>
         </div>
       </div>
     </section>
---
+++ src/media/logos.js
@@ -0,0 +1,7 @@
+/*
+ * Created by Thomas Hartmann
+ * A collection of all the icons. Makes importing them much easier
+ */
+export { default as visittLogo } from './logos/dnb_visitt_1.png'
+export { default as dnbLogo } from './logos/dnb-standard.svg'
+
