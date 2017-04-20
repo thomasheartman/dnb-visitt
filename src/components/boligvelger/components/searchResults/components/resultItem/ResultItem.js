c640069e8dd4d62b6eabe1f9f1af98279a4dda8c
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-04-20 09:49:17 +0200

    Made whole item clickable

--- src/components/boligvelger/components/searchResults/components/resultItem/ResultItem.js
+++ src/components/boligvelger/components/searchResults/components/resultItem/ResultItem.js
@@ -6,37 +6,39 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import defaultImage from '../../../../../../media/images/nybygg.jpg'
 
-export default ({ ProjectName, Poststed, HousingType, Bedrooms, Price, image, altText,  Id,
-fetchProperty = f => f }) => (
-  <article className='housing-item-container'>
-    <div className='housing-item-header row middle-xs'>
-      <h2 className='housing-item-title col-xs start-xs'>{ProjectName}</h2>
-      <span className='housing-item-loc col-xs end-xs'>{Poststed}</span>
-    </div>
-    <div className='row'>
-      <div className='col-xs-6 housing-item-info-container'>
-        <div className='row housing-item-type'>
-          <p>Hustype: {HousingType}</p>
+export default ({ ProjectName, Poststed, HousingType, Bedrooms, Price, image, altText, Id,
+  fetchProperty = f => f }) => (
+    <Link to={{
+      pathname: `/annonse/${Id}`,
+    }}
+      onClick={() => fetchProperty(Id)}
+    >
+
+      <article className='housing-item-container'>
+        <div className='housing-item-header row middle-xs'>
+          <h2 className='housing-item-title col-xs start-xs'>{ProjectName}</h2>
+          <span className='housing-item-loc col-xs end-xs'>{Poststed}</span>
         </div>
-        <div className='row housing-item-rooms'>
-          <p>Rom: {Bedrooms}</p>
+        <div className='row'>
+          <div className='col-xs-6 housing-item-info-container'>
+            <div className='row housing-item-type'>
+              <p>Hustype: {HousingType}</p>
+            </div>
+            <div className='row housing-item-rooms'>
+              <p>Rom: {Bedrooms}</p>
+            </div>
+            <div className='row housing-item-price'>
+              <p>Pris: {Price.toLocaleString()}</p>
+            </div>
+            <div className='row housing-item-read'>
+            </div>
+          </div>
+          <div className='col-xs-6 housing-item-image'>
+            <img className='img-responsive'
+              src={image || defaultImage}
+              alt={altText || ProjectName} />
+          </div>
         </div>
-        <div className='row housing-item-price'>
-          <p>Pris: {Price.toLocaleString()}</p>
-        </div>
-        <div className='row housing-item-read'>
-          <Link to={{
-            pathname: `/annonse/${Id}`,
-          }}
-            onClick={() => fetchProperty(Id)}
-          >Les mer</Link>
-        </div>
-      </div>
-      <div className='col-xs-6 housing-item-image'>
-        <img className='img-responsive'
-          src={image || defaultImage}
-          alt={altText || ProjectName} />
-      </div>
-    </div>
-  </article>
-)
+      </article>
+    </Link>
+  )
