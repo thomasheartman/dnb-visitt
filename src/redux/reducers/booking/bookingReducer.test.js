6b8be43ccf12f4d70f72c20ec20a8937b7468b42
Håvard Ulsbøl <havardulsbol@Havards-MacBook-Pro-2.local>
2017-04-21 15:51:25 +0200

    Test function updates a date in the database, which means the connection is successful. The function still needs work in order to work properly as intended.

--- src/redux/reducers/booking/bookingReducer.test.js
+++ src/redux/reducers/booking/bookingReducer.test.js
@@ -10,11 +10,30 @@ import {
 	database
 } from '../../../firebase/firebase'
 
-it('should pass an arbitrary test', (done) => {
+it('should pass an arbitrary test', () => {
 	const ref = database.ref('appointments')
 
 	ref.on('child_added', snapshot => {
 			console.log(snapshot.val())
 		})
 		//done()
+})
+
+it('should pass an arbitrary test', (done) => {
+	const date = "2017_04_23"
+
+	const ref = database.ref('appointments')
+
+	ref.on('child_added', snapshot => {
+		if (snapshot.val().date !== date) {
+			const post = ref.push()
+			ref.set({
+				date
+			})
+			console.log($(date.val()) + "has been booked.")
+		}
+		else {
+			console.log("The selected date is not available.")
+		}
+	})
 })
