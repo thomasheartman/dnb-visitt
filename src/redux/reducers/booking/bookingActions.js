477f742f4141eeaee64b624343d1a4878a464297
Håvard Ulsbøl <havardulsbol@Havards-MacBook-Pro-2.local>
2017-04-21 14:33:23 +0200

    Small test to see if we can retrieve the appointments. An early attempt to create a push method to the database for appointment dates

--- src/redux/reducers/booking/bookingActions.js
+++ src/redux/reducers/booking/bookingActions.js
@@ -2,3 +2,20 @@
  * Created by Thomas Hartmann
  * Actions for user bookings
  */
+
+import {
+	database
+} from '../../../firebase/firebase'
+
+const bookAvailableDate = (date) => {
+	const ref = database.ref('appointments')
+
+	ref.on('child_Added', snapshot => {
+		if (appointments.date === false) {
+			const post = ref.push()
+			ref.set({
+				date
+			})
+		}
+	})
+}
