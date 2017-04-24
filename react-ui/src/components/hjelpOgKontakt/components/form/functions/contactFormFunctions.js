5f87174f4c340608fede98138198bb3cbfefd1c3
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-04-24 11:10:37 +0200

    Added email handling. Basic connection to firebase.

--- react-ui/src/components/hjelpOgKontakt/components/form/functions/contactFormFunctions.js
+++ react-ui/src/components/hjelpOgKontakt/components/form/functions/contactFormFunctions.js
@@ -3,15 +3,19 @@
  * Functions related to the contact form
  */
 import { composeMail } from '../../../../../mailHandler/mailHandler'
+import { addSupportInquiry } from '../../../../../firebase/queries'
 
 const subject = 'Din henvendelse til DNB Visitt'
 
-const html = (name) => (
+const html = (name, message) => (
   `<h2>Hei, ${name},</h2>
 
   <p>Vi takker for din hendvendelse til DNB Visitt. Vi vil forsøke å svare deg så snart som mulig.</p>
+
+  <p>Din melding: ${message}</p>
   `)
 
-export const confirmEnquiry = ({email, message}) => {
-  composeMail({ to: email, from: 'support@dnb-visitt.no', subject: subject, html: html(message)})
+export const confirmEnquiry = ({ email, name, phoneNumber, message }) => {
+  composeMail({ to: email, from: 'support@dnb-visitt.no', subject: subject, html: html(name, message) })
+  addSupportInquiry(name, email, phoneNumber, message)
 }
