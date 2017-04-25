db6cd8ff30f72a9d8d6e960aebcc9107ac7cc68a
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-04-25 10:58:55 +0200

    Fixed regex

--- react-ui/src/helperFunctions/formatting.js
+++ react-ui/src/helperFunctions/formatting.js
@@ -20,7 +20,7 @@ export const formatDate = (date) => {
 export const toTitleCaseNorwegian = (textString) => {
   const lowerCaseWords = ['og', 'vei', 'gate']
 
-  return textString.replace(/\w\S*/g, (word) => {
+  return textString.replace(/\D\S*/g, (word) => {
     if (lowerCaseWords.includes(word)) return word.toLowerCase()
 
     return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
